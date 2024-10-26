6e567dd26e87a318314b500c9bc1b3587e63a26d
nguyennam1107 <nguyennam22279@gmail.com>
2024-10-26 19:56:57 +0700

    loi khong hien thi Index

--- Index.php
+++ Index.php
@@ -1,19 +1,3 @@
-<html lang="vi">  
-<head>  
-<meta charset="UTF-8">   
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">  
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"  
-    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="  
-    crossorigin="anonymous" referrerpolicy="no-referrer" />  
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>   
-<title>Trang Chủ</title>  
-</head>  
-<body>  
-    <?php include '../View/Navbar.php'; ?>  
-    <?php include '../View/Main.php'; ?>  
-    <?php include '../View/Footer.html'; ?>  
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>  
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>   
-</body>  
-</html>
+<?php include '../View/Navbar.php'; ?>  
+<?php include '../View/Main.php'; ?>  
+<?php include '../View/Footer.html'; ?> 
